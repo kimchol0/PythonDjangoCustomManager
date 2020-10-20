e4397267221164426f78762269a1f7b0f7f67146
KimChol <kimchol2ece@outlook.com>
2020-10-20 22:06:45 +0800

    自定义manager的说明

--- stu/models.py
+++ stu/models.py
@@ -4,4 +4,6 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     sname = models.CharField(max_length=30)
-    indelete = models.BooleanField()
+    indelete = models.BooleanField(default=False)
+
+# student.objects.all() 默认返回全表数据  但是现在打算返回indelete中=true的。这个时候就需要自定义manager

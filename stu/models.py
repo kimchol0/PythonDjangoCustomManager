c4a7a6cfdc3584c4741035f8db4d2b7104dc8509
KimChol <kimchol2ece@outlook.com>
2020-10-21 22:28:11 +0800

    设置Manager

--- stu/models.py
+++ stu/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.db.models.manager import Manager
+
+
+class CustomManager(Manager):
+    def all(self):
+        return Manager.all(self).filter(isdelete=True)
 
 
 # Create your models here.
 class Student(models.Model):
     sname = models.CharField(max_length=30)
     isdelete = models.BooleanField(default=False)
+    objects = CustomManager()
 
+    def __str__(self):
+        return u'Student:%s'%self.isdelete
 # Student.objects.all() 默认返回全表数据  但是现在打算返回indelete中=true的。这个时候就需要自定义manager

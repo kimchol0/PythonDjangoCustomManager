207067f2c2dd0efcee9bab6c46b62ae5daa10d4d
KimChol <kimchol2ece@outlook.com>
2020-10-21 21:51:56 +0800

    模型文件错误修改

--- stu/models.py
+++ stu/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     sname = models.CharField(max_length=30)
-    indelete = models.BooleanField(default=False)
+    isdelete = models.BooleanField(default=False)
 
 # Student.objects.all() 默认返回全表数据  但是现在打算返回indelete中=true的。这个时候就需要自定义manager

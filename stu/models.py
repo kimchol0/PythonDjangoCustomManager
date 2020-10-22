5cc64087fcfdc31fd1239c45496a3f0d25b48353
KimChol <kimchol2ece@outlook.com>
2020-10-22 22:16:43 +0800

    自定义manager显示false的结果

--- stu/models.py
+++ stu/models.py
@@ -7,12 +7,19 @@ class CustomManager(Manager):
         return Manager.all(self).filter(isdelete=True)
 
 
+class NoDeletedManager(Manager):
+    def all(self):
+        return Manager.all(self).filter(isdelete=False)
+
+
 # Create your models here.
 class Student(models.Model):
     sname = models.CharField(max_length=30)
     isdelete = models.BooleanField(default=False)
     objects = CustomManager()
 
+    show = NoDeletedManager()
+
     def __str__(self):
-        return u'Student:%s'%self.isdelete
+        return u'Student:%s' % self.isdelete
 # Student.objects.all() 目前返回isdelete=True得了

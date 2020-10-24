0d00554c957ecb4b2750195b1122484603068723
KimChol <kimchol2ece@outlook.com>
2020-10-24 22:23:11 +0800

    为保证之后提交程序的稳定运行，删除187b749,ebf280e,60e8b2b历史提交记录代码。

--- stu/models.py
+++ stu/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import QuerySet
 from django.db.models.manager import Manager
-from new
 
 class CustomManager(Manager):
     def get_queryset(self):
@@ -12,31 +11,10 @@ class NoDeletedManager(Manager):
     def all(self):
         return Manager.all(self).filter(isdelete=False)
 
-
-class BatchDelManager(Manager):
-    def get_queryset(self):
-        return Manager.get_queryset(self).filter(isdelete=False)
-
-    def filter(self, *args, **kwargs):
-        # 获取需要删除记录
-        delList = Manager.get_queryset(self)
-
-        # 定义闭包方法执行修改isdelete=True操作
-        def delete1(delqueryset):
-            for dq in delqueryset:
-                dq.isdelete = True
-                dq.save()
-            delList.delete = new.instancemethod(delete1, delList, QuerySet)
-
-        return delList
-
-
 # Create your models here.
 class Student(models.Model):
     sname = models.CharField(max_length=30)
     isdelete = models.BooleanField(default=False)
-
-    objects = BatchDelManager()
 
     def __str__(self):
         return u'Student:%s' % self.isdelete
